213b02c470b79aff10c9c9372e3036a5074199af
whigy <whigy9@gmail.com>
2016-12-12 14:25:37 +0100

    Part 5

--- Lab3/mixture_prob.m
+++ Lab3/mixture_prob.m
@@ -1,37 +1,12 @@
 function prob = mixture_prob(image, K, L, mask)
 %MIXTURE_PROB
 %Let I be a set of pixels and V be a set of K Gaussian components in 3D (R,G,B).
-%%
-% clear
-% clc
-% 
-% scale_factor = 0.5;          % image downscale factor
-% area = [ 80, 110, 570, 300 ] % image region to train foreground with
-% K = 16;                      % number of mixture components
-% alpha = 8.0;                 % maximum edge cost
-% sigma = 10.0;                % edge cost decay factor
-% L = 10;
-% 
-% I = imread('tiger1.jpg');
-% I = imresize(I, scale_factor);
-% Iback = I;
-% area = int16(area*scale_factor);
-% image = I ;
-%%
+
 image = im2double(image);
 [m, n, d] = size(image);
 
-% %%%%discard
-% mask = ones(m, n );
-% %%%%%%%%%%%%
-
 I = reshape(image, m * n, d);
 mask = reshape(mask, m * n, 1);
-
-% %%%%discard
-% ind = randperm(m * n);
-% mask(ind(1 : 30000), :) = 0;
-% %%%%%%%%%%%%%
 
 % Store all pixels for which mask=1 in a Nx3 matrix
 Imask = I(mask == 1, :);
@@ -39,8 +14,9 @@ Imask = I(mask == 1, :);
 % Randomly initialize the K components using masked pixels
 n_mask = size(Imask, 1);
 disp(['size after mask: ', num2str(n_mask)]);
-%ind = randperm(n_mask);
+
 w = ones(1, K) /K;
+%ind = randperm(n_mask); % Another way of initialization of mu
 %mu = Imask( ind(1:K), :);
 [ foo, mu ] = kmeans_segm3(image, K, 25, 14, 0); % Initialize with K-means center, hopefully get a faster converge
 sigma = repmat( eye(3), 1, K);
@@ -48,7 +24,7 @@ sigma = reshape(sigma, 3, 3, K);
 sigma = 0.04 * rand * sigma;
 
 P = zeros( n_mask, K);
-%%
+
 % Iterate L times
 for l = 1 : L
 %   Expectation: Compute probabilities P_ik using masked pixels
@@ -56,7 +32,7 @@ for l = 1 : L
         dif = bsxfun(@minus, Imask, mu(k, :)); %size(dif) = [n_mask , 3]
         cova = sigma( :, :,k);
         
-        g_k = 1 / sqrt( (2*pi)^3 * det(cova)) * exp ( -0.5 * sum(dif /cova .* dif,2)); % size(g_k) = [n_mask , 1]
+        g_k = (1 / sqrt((2 * pi)^3 * abs(det(cova)))) * exp ( -0.5 * sum(dif /cova .* dif,2)); % size(g_k) = [n_mask , 1]
         P(:, k) = w(k) * g_k;
     end
     P = bsxfun(@rdivide, P, sum(P,2));
@@ -66,27 +42,27 @@ for l = 1 : L
     w = sum(P) / n_mask;     %What is N?
     
     % update mu
-    mu = P' * Imask ./ repmat(sum(P)', 1, 3);
+    mu = P' * Imask ./ repmat(sum(P)', 1, 3); 
     
     % update sigma
     for k = 1 : K
         dif = bsxfun(@minus, mu(k, :), Imask);
-        s = bsxfun(@times, dif, P(:, k))' * dif;
-        sigma (:, :, k) = s ./ sum(P(:, k));
+        s = bsxfun(@times, dif, P(:, k))' * dif; 
+        sigma (:, :, k) = s ./ sum(P(:, k)) + 0.0005 * diag(diag(rand(3))); %The trick is necessary, but why?
     end
 end
     
 %   Compute probabilities p(c_i) in Eq.(3) for all pixels I.
-prob = zeros( m * n, 1);
+
+P = zeros( m * n, K); 
 for k = 1 :K
-    dif = bsxfun(@minus, Imask, mu(k, :));
+    dif = bsxfun(@minus, I, mu(k, :));
     cova = sigma( :, :,k);
 
     g_k = 1 / sqrt( (2*pi)^3 * det(cova)) * exp ( -0.5 * sum(dif /cova .* dif,2));
     P(:, k) = w(k) * g_k;
 end
-prob_mask = sum(P,2);
-prob(mask == 1, :) = prob_mask;
+prob = sum(P,2);
 prob = reshape(prob, m, n, 1);
 end
 
